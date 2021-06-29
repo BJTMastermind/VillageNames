d7eb685042ee8afd6fcd6ef0a9c5d7b749ec3452
AstroTibs <tibs.chris@gmail.com>
2021-06-29 05:28:21 -0500

    Update to 4.3.1

--- src/main/java/astrotibs/villagenames/utility/Reference.java
+++ src/main/java/astrotibs/villagenames/utility/Reference.java
@@ -3,17 +3,16 @@ package astrotibs.villagenames.utility;
 public class Reference
 {
 	// Contains common constants for the mod
-	public static final String
-	MOD_ID = "villagenames",
-	MOD_NAME = "Village Names",
-	VERSION = "4.3.0",
-	URL = "https://www.curseforge.com/minecraft/mc-mods/village-names",
-	VERSION_CHECKER_URL = "https://gitgud.io/AstroTibs/VillageNames/raw/1.11.2/CURRENT_VERSION",
-	MOD_CHANNEL = "vnChannel",
-	CLIENT_PROXY = "astrotibs.villagenames.proxy.ClientProxy",
-	SERVER_PROXY = "astrotibs.villagenames.proxy.ServerProxy",
-	COMMON_PROXY = "astrotibs.villagenames.proxy.CommonProxy",
-	GUI_FACTORY = "astrotibs.villagenames.config.gui.VNGuiFactory";
+	public static final String MOD_ID = "villagenames";
+	public static final String MOD_NAME = "Village Names";
+	public static final String VERSION = "4.3.1";
+	public static final String URL = "https://www.curseforge.com/minecraft/mc-mods/village-names";
+	public static final String VERSION_CHECKER_URL = "https://gitgud.io/AstroTibs/VillageNames/raw/1.11.2/CURRENT_VERSION";
+	public static final String MOD_CHANNEL = "vnChannel";
+	public static final String CLIENT_PROXY = "astrotibs.villagenames.proxy.ClientProxy";
+	public static final String SERVER_PROXY = "astrotibs.villagenames.proxy.ServerProxy";
+	public static final String COMMON_PROXY = "astrotibs.villagenames.proxy.CommonProxy";
+	public static final String GUI_FACTORY = "astrotibs.villagenames.config.gui.VNGuiFactory";
 	
     public static final String VILLAGER_CLASS = "net.minecraft.entity.passive.EntityVillager";
     public static final int STREET_WIDTH = 3;
